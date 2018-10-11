f57b22f44184477f6ce8eba4ac540a784ef82e3a
Guy Thomas <gthomas@reflektive.com>
2018-10-11 15:48:50 -0700

    Add sprite and gravity

--- src/main.js
+++ src/main.js
@@ -1,23 +1,21 @@
-import 'phaser';
-import GameScene from './scenes/GameScene';
+import "phaser";
+import GameScene from "./scenes/GameScene";
 
 const config = {
     type: Phaser.WEBGL,
     pixelArt: true,
     roundPixels: true,
-    parent: 'test',
-    width: 400,
-    height: 240,
+    parent: "test",
+    width: 1000,
+    height: 1000,
     physics: {
-        default: 'arcade',
+        default: "arcade",
         arcade: {
             gravity: { y: 800 },
             debug: false
         }
     },
-    scene: [
-        GameScene
-    ]
+    scene: [GameScene]
 };
 
 const game = new Phaser.Game(config);
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,18 +1,66 @@
-import Phaser from 'phaser';
-
+import Phaser from "phaser";
+import { PLAYER, BACKGROUND } from "../constants";
 class GameScene extends Phaser.Scene {
     constructor(test) {
         super({
-            key: 'GameScene'
+            key: "GameScene"
+        });
+        this.player = null;
+    }
+
+    addPlayer() {
+        this.player = this.physics.add.sprite(100, 450, PLAYER);
+
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: "left",
+            frames: this.anims.generateFrameNumbers(PLAYER, {
+                start: 0,
+                end: 3
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "turn",
+            frames: [{ key: PLAYER, frame: 4 }],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: "right",
+            frames: this.anims.generateFrameNumbers(PLAYER, {
+                start: 5,
+                end: 8
+            }),
+            frameRate: 10,
+            repeat: -1
         });
     }
 
+    addPlatforms() {
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms
+            .create(400, 568, "ground")
+            .setScale(2)
+            .refreshBody();
+    }
+
     preload() {
-        this.load.image('background', 'assets/images/background.png');
+        this.load.image(BACKGROUND, "assets/images/background.png");
+        this.load.image("ground", "assets/images/platform.png");
+        this.load.spritesheet(PLAYER, "assets/images/player.png", {
+            frameWidth: 32,
+            frameHeight: 48
+        });
     }
 
     create() {
-        this.add.sprite(400, 240, 'background');
+        this.add.sprite(400, 240, "background");
+        this.addPlayer();
     }
 }
 
